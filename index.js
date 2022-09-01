7e1aee51ac042174fa2ff03bf042a5d306036710
Dalonna456 <89915840+Dalonna456@users.noreply.github.com>
2022-09-01 19:31:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+const timerEl = document.getElementById('timer');
+
+console.log(timerEl);
+
+const startingMinutes = 60;
+let time = startingMinutes * 60;
+
+setInterval(updateTimer, 1000);
+
+function updateTimer() {
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    timerEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
+
+
